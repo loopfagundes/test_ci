177823c6bc33775b82d10bc966438d205cf3adbf
Luciano Oliveira Fagundes <luciano.fagundes@dbserver.com.br>
2019-11-13 16:50:36 -0300

    New Framework DriverManageFactoryFw, DriverTypeFw, BaseTestFw...BaseTest dentro Bafore e Atfer

--- src/test/java/testcases/CompraTestCase.java
+++ src/test/java/testcases/CompraTestCase.java
@@ -1,40 +1,22 @@
 package testcases;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import framework.BaseTestFw;
 import tasks.HomeTask;
 import tasks.LoginTask;
 
-public class CompraTestCase {
+public class CompraTestCase extends BaseTestFw {
 	
-	private static WebDriver driver;
-	private static LoginTask loginTask;
-	private static HomeTask homeTask;
+	private WebDriver driver = getDriver();
+	private LoginTask loginTask = new LoginTask(driver);
+	private HomeTask homeTask = new HomeTask(driver);
 	
-	@BeforeClass
-	public static void setUp() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.get("http://automationpractice.com/index.php");
-		driver.manage().window().maximize();
-		loginTask = new LoginTask(driver);	
-		homeTask = new HomeTask(driver);
-	}
 	
 	@Test
 	public void test () {
+		driver.get("http://automationpractice.com/index.php");
 		loginTask.login("luk.ninja.91@gmail.com", "d8i5d2i0");
 		homeTask.home("dress");
 	}
-	
-	@AfterClass
-	public static void tearDown() {
-		driver.quit();
-	}
-
 }
